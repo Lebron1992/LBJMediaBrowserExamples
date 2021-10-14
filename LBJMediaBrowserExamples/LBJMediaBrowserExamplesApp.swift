a859821ce596dff3fae7db363dbc12ff82faf62d
Lebron <wenzhi.zeng@outlook.com>
2021-10-14 11:56:40 +0800

    Initial Commit

---
+++ LBJMediaBrowserExamples/LBJMediaBrowserExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBJMediaBrowserExamplesApp.swift
+//  LBJMediaBrowserExamples
+//
+//  Created by Lebron Zeng on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct LBJMediaBrowserExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
