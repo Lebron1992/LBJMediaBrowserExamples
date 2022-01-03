1a580c42cd0db88f4356225fb3a33368e6339b34
Lebron <wenzhi.zeng@outlook.com>
2022-01-03 09:40:44 +0800

    update demo

--- LBJMediaBrowserExamples/LBJMediaBrowserExamplesApp.swift
+++ LBJMediaBrowserExamples/LBJMediaBrowserExamplesApp.swift
@@ -1,10 +1,15 @@
 import SwiftUI
+import LBJMediaBrowser
 
 @main
 struct LBJMediaBrowserExamplesApp: App {
+
+  let mediaBrowserEnvironment = LBJMediaBrowserEnvironment()
+
   var body: some Scene {
     WindowGroup {
       ContentView()
+        .environment(\.mediaBrowserEnvironment, mediaBrowserEnvironment)
     }
   }
 }
