4df2a1de6fa83dd86de8bf17d180ec2035d22c1e
Lebron <wenzhi.zeng@outlook.com>
2022-01-24 22:25:30 +0800

    update demo

--- LBJMediaBrowserExamples/LBJMediaBrowserExamplesApp.swift
+++ LBJMediaBrowserExamples/LBJMediaBrowserExamplesApp.swift
@@ -4,7 +4,22 @@ import LBJMediaBrowser
 @main
 struct LBJMediaBrowserExamplesApp: App {
 
-  let mediaBrowserEnvironment = LBJMediaBrowserEnvironment()
+  let mediaBrowserEnvironment: LBJMediaBrowserEnvironment = {
+    let imageCache: ImageCache? = {
+      let diskStorage = try? ImageDiskStorage(config: .init(
+        name: "ImageCache",
+        sizeLimit: 0,
+        expiration: .days(7)
+      ))
+      let memoryStorage = ImageMemoryStorage(
+        memoryCapacity: 100_000_000,
+        preferredMemoryCapacityAfterPurge: 80_000_000
+      )
+      let cache = ImageCache(diskStorage: diskStorage, memoryStorage: memoryStorage)
+      return cache
+    }()
+    return LBJMediaBrowserEnvironment(imageCache: imageCache)
+  }()
 
   var body: some Scene {
     WindowGroup {
